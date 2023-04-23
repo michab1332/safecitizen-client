05ee914ba9200894719a046793d350fc4378637e
Michal Bonowicz <bonowiczmichal1332@gmail.com>
2023-04-23 22:47:43 +0200

    code: delete unnecessary files and coments

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -10,13 +10,6 @@ const Header = () => {
     const { user } = useSelector(state => state.user);
     const dispatch = useDispatch();
 
-    const [isVisible, setIsVisible] = useState(false);
-
-    const handleChangeVisibleOnClick = (e) => {
-        e.preventDefault();
-        setIsVisible(prevState => !prevState);
-    }
-
     const handleLogout = () => {
         dispatch(logout());
     }
@@ -29,10 +22,6 @@ const Header = () => {
             </div>
             <Link className={user ? "header__link-hidden" : "header__link"} to="/signin">Zaloguj się</Link>
             <p onClick={handleLogout} className={user ? "header__link" : "header__link-hidden"}>Wyloguj się</p>
-            {/* <div onClick={e => handleChangeVisibleOnClick(e)} className="header-burger">
-                    <img src={Burger} alt="burger" />
-                </div> */}
-            {/* <Menu isVisible={isVisible} handleChangeVisibleOnClick={handleChangeVisibleOnClick} /> */}
         </header>
     )
 }
